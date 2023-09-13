37d69d7e6e38612e57ca320d4af8fa4ff5ef8cc7
ninetyninereds <idosh@kooply.com>
2023-09-13 16:22:26 +0300

    copy gradle folder

--- dist/platforms/ubuntu/steps/set_gradle_cache.sh
+++ dist/platforms/ubuntu/steps/set_gradle_cache.sh
@@ -2,4 +2,24 @@
 
 echo "*********************************** Updating the Gradle Cache: ****************************************"
 ls -la ~/.gradle 2>/dev/null
+
+#!/bin/bash
+
+# Define the source and target directories
+SOURCE_DIR="${HOME}/.gradle"
+TARGET_DIR="./GradleCache"
+
+# Check if the source directory exists
+if [[ -d "${SOURCE_DIR}" ]]; then
+    # Ensure the target directory exists; create if it doesn't
+    mkdir -p "${TARGET_DIR}"
+
+    # Try to copy the contents from the source to the target
+    cp -R "${SOURCE_DIR}/." "${TARGET_DIR}/" && echo "Successfully copied contents." || echo "Failed to copy contents."
+else
+    echo "Source directory '${SOURCE_DIR}' does not exist. Nothing to copy."
+fi
+
+ls -la
+
 echo "*******************************************************************************************************"
