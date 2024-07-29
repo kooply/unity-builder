b33945424ef33c7af225a6f6b1648310fe1d4491
ninetyninereds <idosh@kooply.com>
2024-07-29 17:59:48 +0300

    no sudo

--- dist/platforms/ubuntu/steps/cocoapods.sh
+++ dist/platforms/ubuntu/steps/cocoapods.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 
-sudo apt-get install curl g++ gcc autoconf automake bison libc6-dev \
+apt-get install curl g++ gcc autoconf automake bison libc6-dev \
                      libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool \
                      libyaml-dev make patch pkg-config sqlite3 zlib1g-dev libgmp-dev \
-                     libreadline-dev libssl-dev
+                     libreadline-dev libssl-dev gpg sudo
 
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable
 
-sudo usermod -a -G rvm root
+usermod -a -G rvm root
 
 id
 
@@ -28,7 +28,7 @@ echo "==================== Selecting Ruby ======================="
 rvm --default use ruby-3.1.0
 
 echo "==================== Installing Cocoa Pods ======================="
-sudo gem install cocoapods
+gem install cocoapods
 
 pod setup
 
